56d79c266af5708b2fa1fc08aca090109a032ff9
Selvachandru58 <Selvachandru17@gmail.com>
2025-05-13 19:36:16 +0530

    fixx

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   base: process.env.VITE_BASE_PATH || "/tripsPlan",
 })
